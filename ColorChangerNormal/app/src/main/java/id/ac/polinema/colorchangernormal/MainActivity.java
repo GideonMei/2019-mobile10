0b08801c42c58560a057f48c392c138fb0d5eec5
GideonMei <mei.gideon.gm@gmail.com>
2019-10-30 14:53:43 +0700

    Merubah warna percobaan 1

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -2,17 +2,21 @@ package id.ac.polinema.colorchangernormal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-	private ConstraintLayout rootView;
-	private Button btnChangeColor;
+	ConstraintLayout rootView;
+	Button btnChangeColor;
+
+	ColorViewModel colorViewModel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,20 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		rootView = findViewById(R.id.layout_main);
 		btnChangeColor = findViewById(R.id.change_color);
-		
+		colorViewModel = ViewModelProviders.of(this).get(ColorViewModel.class);
+		rootView.setBackgroundColor(colorViewModel.getColor());
 		rootView.setBackgroundColor(generateRandomColor());
 
 		// Tambahkan event klik pada tombol
+		btnChangeColor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				int color = generateRandomColor();
+				rootView.setBackgroundColor(color);
+				rootView.setBackgroundColor(colorViewModel.getColor());
+				colorViewModel.setColor(color);
+			}
+		});
 	}
 
 	private int generateRandomColor(){
